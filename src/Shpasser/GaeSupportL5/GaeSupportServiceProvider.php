8a9d85e5a9dd348e1a13695990ec2871317e9a7e
Ron Shpasser <shpasser@gmail.com>
2015-02-16 08:48:41 +0000

    Added GCS bucket filesystem adapter

--- src/Shpasser/GaeSupportL5/GaeSupportServiceProvider.php
+++ src/Shpasser/GaeSupportL5/GaeSupportServiceProvider.php
@@ -1,9 +1,10 @@
 <?php namespace Shpasser\GaeSupportL5;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\Filesystem as Flysystem;
 use Shpasser\GaeSupportL5\Setup\SetupCommand;
-use Shpasser\GaeSupportL5\Filesystem\GaeAdapter as GaeFileSystemAdapter;
-use Storage;
+use Shpasser\GaeSupportL5\Filesystem\GaeAdapter as GaeFilesystemAdapter;
 
 class GaeSupportServiceProvider extends ServiceProvider {
 
@@ -21,9 +22,9 @@ class GaeSupportServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        Storage::extend('gae', function($app)
+        Storage::extend('gae', function($app, $config)
         {
-            return Storage::repository(new GaeFilesystemAdapter);
+            return new Flysystem(new GaeFilesystemAdapter($config['root']));
         });
     }
 
